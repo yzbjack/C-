385ee0e66418339d40c4dfd8c17bf19a34250d12
yzbjack <30550297+yzbjack@users.noreply.github.com>
2019-03-16 19:34:41 +0800

    Add files via upload

---
+++ 客服端8.0GUI/客服端8.0GUI/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.IO;
+
+
+
+namespace 客服端8._0GUI
+{
+    public partial class Form1 : Form
+
+    {
+        private byte[] result = new byte[1024 * 1024];
+        private Socket ClientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        public Form1()
+        {
+            InitializeComponent();
+            TextBox.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            ;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Thread.Sleep(2000);
+                richTextBox1.Text += "向服务器发送消息:\r\n";
+                richTextBox1.Text += textBox1.Text + "\r\n";
+                string sb = textBox1.Text;
+                string Client = ClientSocket.RemoteEndPoint.ToString();
+                string SendMessage = "接收客户端" + Client + "消息：" + DateTime.Now + "\r\n" + sb + "\r\n";
+                ClientSocket.Send(Encoding.UTF8.GetBytes(sb));
+                textBox1.Clear();
+            }
+            catch (Exception ex)
+            {
+                richTextBox1.Text += "服务器可能已经关闭！\r\n";
+                ClientSocket.Shutdown(SocketShutdown.Both);
+                ClientSocket.Close();
+            }
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int Port = Convert.ToInt32(textBox3.Text);
+            IPAddress IP = IPAddress.Parse((string)textBox2.Text);
+            try
+            {
+                ClientSocket.Connect(new IPEndPoint(IP, Port));
+                richTextBox2.Text += "连接服务器成功！\r\n";
+                Thread thread = new Thread(ReceiveMessage);
+                thread.IsBackground = true;
+                thread.Start();
+            }
+            catch (Exception ex)
+            {
+                richTextBox2.Text += "连接服务器失败！\r\n";
+                return;
+            }
+           
+            
+        }
+        private void ReceiveMessage()
+        {
+            while (true)
+            {
+                try
+                {
+                    int ReceiveLength = ClientSocket.Receive(result);
+                    richTextBox2.Text += "接收服务器消息：\r\n";
+                    string str = Encoding.UTF8.GetString(result, 0, ReceiveLength) + "\r\n";
+                    richTextBox2.Text += str;
+                }
+                catch (Exception ex)
+                {
+                    richTextBox2.Text += "接收消息失败！\r\n";
+                    ClientSocket.Shutdown(SocketShutdown.Both);
+                    ClientSocket.Close();
+                    break;
+                }
+            }
+        }
+
+    }
+}
---
+++ 聊天室5.0/聊天室5.0/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text;
+using System.Linq;
+namespace Server
+{
+    class Program
+    {
+        public static void ListenConnection()
+        {
+            Socket ConnectionSocket = null;
+            while (true)
+            {
+                try
+                {
+                    ConnectionSocket = ServerSocket.Accept();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("监听套接字异常{0}", ex.Message);
+                    break;
+                }
+                //获取客户端端口号和IP
+                IPAddress ClientIP = (ConnectionSocket.RemoteEndPoint as IPEndPoint).Address;
+                int ClientPort = (ConnectionSocket.RemoteEndPoint as IPEndPoint).Port;
+                string SendMessage = "连接服务器成功\r\n" + "本地IP:" + ClientIP +
+                    ",本地端口:" + ClientPort.ToString();
+                //Console.WriteLine(SendMessage);
+                ConnectionSocket.Send(Encoding.UTF8.GetBytes(SendMessage));
+                string remotePoint = ConnectionSocket.RemoteEndPoint.ToString();
+                Console.WriteLine("成功与客户端{0}建立连接!\t\n", remotePoint);
+                ClientInformation.Add(remotePoint, ConnectionSocket);
+                Thread thread = new Thread(ReceiveMessage);
+                thread.IsBackground = true;
+                thread.Start(ConnectionSocket);
+            }
+        }
+        public static void ReceiveMessage(Object SocketClient)
+        {
+            Socket ReceiveSocket = (Socket)SocketClient;
+            while (true)
+            {
+                byte[] result = new byte[1024 * 1024];
+                try
+                {
+                    IPAddress ClientIP = (ReceiveSocket.RemoteEndPoint as IPEndPoint).Address;
+                    int ClientPort = (ReceiveSocket.RemoteEndPoint as IPEndPoint).Port;
+                    int ReceiveLength = ReceiveSocket.Receive(result);
+                    string ReceiveMessage = Encoding.UTF8.GetString(result, 0, ReceiveLength);
+                    Console.WriteLine("接收客户端:" + ReceiveSocket.RemoteEndPoint.ToString() +
+                        "时间：" + DateTime.Now.ToString() + "\r\n" + "消息：" + ReceiveMessage + "\r\n\n");
+                    foreach (string key in new List<string>(ClientInformation.Keys))
+                    {
+                        string s = ReceiveSocket.RemoteEndPoint.ToString();
+                        if (key != s)
+                        {
+                            Socket socket = ClientInformation[key];
+                            Console.WriteLine("向客户端{0}发送消息：", key);
+                            socket.Send(Encoding.UTF8.GetBytes(ReceiveMessage));
+                        }
+                    }
+                    //Console.WriteLine("向客户端{0}发送消息：", ReceiveSocket.RemoteEndPoint.ToString());
+                    //string sb = Console.ReadLine();
+                    //ReceiveSocket.Send(Encoding.UTF8.GetBytes(sb));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("监听出现异常!!!");
+                    Console.WriteLine("客户端" + ReceiveSocket.RemoteEndPoint + "已经连接中断" + "\r\n" +
+                        ex.Message + "\r\n" + ex.StackTrace + "\r\n");
+                    foreach (string key in new List<string>(ClientInformation.Keys))
+                    {
+                        string s = ReceiveSocket.RemoteEndPoint.ToString();
+                        if (key.Equals(s))
+                        {
+                            ClientInformation.Remove(key);
+                        }
+                    }
+                    ReceiveSocket.Shutdown(SocketShutdown.Both);
+                    ReceiveSocket.Close();
+                    break;
+                }
+            }
+        }
+        //private static byte[] result = new byte[1024];
+        //List<Socket> list = new List<Socket>();
+        static Dictionary<string, Socket> ClientInformation = new Dictionary<string, Socket> { };
+        static Socket ServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        static void Main(String[] args)
+        {
+            try
+            {
+                int Port = 8899;
+                IPAddress IP = IPAddress.Parse("127.0.0.1");
+                ServerSocket.Bind(new IPEndPoint(IP, Port));
+                ServerSocket.Listen(10);
+                Console.WriteLine("启动监听成功！");
+                Console.WriteLine("监听本地{0}成功", ServerSocket.LocalEndPoint.ToString());
+                Thread ThreadListen = new Thread(ListenConnection);
+                ThreadListen.IsBackground = true;
+                ThreadListen.Start();
+                Thread.Sleep(2000);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("监听异常!!!");
+                ServerSocket.Shutdown(SocketShutdown.Both);
+                ServerSocket.Close();
+            }
+            Console.ReadKey();
+            Console.WriteLine("监听完毕，按任意键退出！");
+        }
+    }
+}
+//--------------------- 
+//作者：童话ing
+//来源：CSDN
+//原文：https://blog.csdn.net/dl962454/article/details/78990735 
+//版权声明：本文为博主原创文章，转载请附上博文链接！
